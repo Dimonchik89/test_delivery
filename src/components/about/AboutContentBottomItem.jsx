d5414d88f42bd3999d70b226c832a01afbf297ac
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2025-04-19 11:28:24 +0300

    change about content bottom item

--- src/components/about/AboutContentBottomItem.jsx
+++ src/components/about/AboutContentBottomItem.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const AboutContentBottomItem = ({ Icon, title, subtitle }) => {
 	return (
 		<div className="flex about-bottom__item">
-			<Icon />
-			<div className="flex flex-col ">
-				<p className="about-bottom__subtitle">{subtitle}</p>
-				<p className="about-bottom__title">{title}</p>
+			<div className="flex about-bottom__item-wrapper">
+				<Icon />
+				<div className="flex flex-col about-bottom__item-info">
+					<p className="about-bottom__subtitle">{subtitle}</p>
+					<p className="about-bottom__title">{title}</p>
+				</div>
 			</div>
 		</div>
 	);
